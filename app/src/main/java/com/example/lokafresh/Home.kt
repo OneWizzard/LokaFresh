9e83645467f1a2c2638e4c2f8b381b1684bdc945
Adimas <admsfarhan@gmail.com>
2025-04-13 19:45:17 +0700

    Home, Recyler view, BotNav implement

--- app/src/main/java/com/example/lokafresh/Home.kt
+++ app/src/main/java/com/example/lokafresh/Home.kt
@@ -3,12 +3,14 @@ package com.example.lokafresh
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class Login : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_home)
 
         }
     }
