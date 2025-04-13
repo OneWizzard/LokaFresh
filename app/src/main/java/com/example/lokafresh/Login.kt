0ff60d3874569915a1a01f5d323688df1548bf56
OneWizard <kurniawanrachmadhani@gmail.com>
2025-04-13 20:34:35 +0700

    minor fixing & alrdy can running

--- app/src/main/java/com/example/lokafresh/Login.kt
+++ app/src/main/java/com/example/lokafresh/Login.kt
@@ -1,6 +1,8 @@
 package com.example.lokafresh
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +12,11 @@ class Login : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+        }
         }
     }
