2fc4e53672e394af6fbc8e9a1a62249938f12056
OneWizard <kurniawanrachmadhani@gmail.com>
2025-05-10 10:26:08 +0700

    uploud 2 menit 2 gambar

--- app/src/main/java/com/example/lokafresh/retrofit/ApiConfig.kt
+++ app/src/main/java/com/example/lokafresh/retrofit/ApiConfig.kt
@@ -1,15 +1,24 @@
 package com.example.lokafresh.retrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiConfig {
 
     private const val BASE_URL = "http://34.143.173.201:8502/"
 
     fun getApiService(): ApiService {
+        val client = OkHttpClient.Builder()
+            .connectTimeout(120, TimeUnit.SECONDS) // Timeout saat membuat koneksi
+            .readTimeout(120, TimeUnit.SECONDS)    // Timeout saat membaca respon
+            .writeTimeout(120, TimeUnit.SECONDS)   // Timeout saat menulis data
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
